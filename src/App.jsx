cf49cb919ebafca337d610de8cf0e2621e636be4
ahmadytou <mahy609@gmail.com>
2025-01-17 16:46:52 +0500

    testing

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { Button } from "@material-tailwind/react";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Button>Button</Button>
+  )
+}
+
+export default App
